a666c556087a90d6da8dc39c32c2dd84b9e4c22c
Taislin <cariosecus@gmail.com>
2022-08-17 01:11:15 +0100

    style fixes

--- scripts/webpack.common.js
+++ scripts/webpack.common.js
@@ -2,6 +2,7 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 	context: path.resolve("__dirname", "../"),
@@ -16,9 +17,31 @@ module.exports = {
 		new CopyWebpackPlugin({
 			patterns: ["./client/src/login.html", "./client/src/client.html", "./client/src/favicon.ico"],
 		}),
+		new MiniCssExtractPlugin(),
 	],
 	output: {
 		filename: "[name].bundle.js",
 		path: path.resolve(__dirname, "../client/public"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+			// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+			{
+				test: /\.js$/,
+				include: [path.resolve(__dirname, "src", "client")],
+				use: [
+					{
+						loader: "babel-loader",
+						options: {
+							plugins: [["import", { libraryName: "antd", style: true }, "antd"]],
+						},
+					},
+				],
+			},
+		],
 	},
 };
