fcacb6706838a0e96e42c3ac26b9d950de67a6d3
Taislin <cariosecus@gmail.com>
2022-09-08 20:04:14 +0100

    removes unused code

--- client/src/index.js
+++ client/src/index.js
@@ -3,21 +3,12 @@ import { io } from "socket.io-client";
 import { Terminal } from "xterm";
 import { FitAddon } from "xterm-addon-fit";
 
-let sessionLogEnable = false;
-let loggedData = false;
-let sessionLog;
 let sessionFooter;
-let logDate;
 let currentDate;
 let myFile;
 let errorExists;
 const term = new Terminal();
 // DOM properties
-const logBtn = document.getElementById("logBtn");
-const downloadLogBtn = document.getElementById("downloadLogBtn");
-const adjustBtn = document.getElementById("adjustBtn");
-const rowsInput = document.getElementById("rowsInput");
-const colsInput = document.getElementById("colsInput");
 const status = document.getElementById("status");
 const header = document.getElementById("header");
 const footer = document.getElementById("footer");
@@ -33,89 +24,6 @@ const socket = io({
 	path: "/socket.io",
 });
 
-// cross browser method to "download" an element to the local system
-// used for our client-side logging feature
-function downloadLog() {
-	// eslint-disable-line
-	if (loggedData === true) {
-		myFile = `SSHnake-${logDate.getFullYear()}${
-			logDate.getMonth() + 1
-		}${logDate.getDate()}_${logDate.getHours()}${logDate.getMinutes()}${logDate.getSeconds()}.log`;
-		// regex should eliminate escape sequences from being logged.
-		const blob = new Blob(
-			[
-				sessionLog.replace(
-					// eslint-disable-next-line no-control-regex
-					/[\u001b\u009b][[\]()#;?]*(?:\d{1,4}(?:;\d{0,4})*)?[0-9A-ORZcf-nqry=><;]/g,
-					""
-				),
-			],
-			{
-				// eslint-disable-line no-control-regex
-				type: "text/plain",
-			}
-		);
-		const elem = window.document.createElement("a");
-		elem.href = window.URL.createObjectURL(blob);
-		elem.download = myFile;
-		document.body.appendChild(elem);
-		elem.click();
-		document.body.removeChild(elem);
-	}
-	term.focus();
-}
-// Set variable to toggle log data from client/server to a varialble
-// for later download
-function toggleLog() {
-	// eslint-disable-line
-	if (sessionLogEnable === true) {
-		sessionLogEnable = false;
-		loggedData = true;
-		logBtn.innerHTML = "Start Log";
-		currentDate = new Date();
-		sessionLog = `${sessionLog}\r\n\r\nLog End for ${sessionFooter}: ${currentDate.getFullYear()}/${
-			currentDate.getMonth() + 1
-		}/${currentDate.getDate()} @ ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}\r\n`;
-		logDate = currentDate;
-		term.focus();
-	} else {
-		sessionLogEnable = true;
-		loggedData = true;
-		logBtn.innerHTML = "Stop Log";
-		downloadLogBtn.style.color = "#000";
-		downloadLogBtn.addEventListener("click", downloadLog);
-		currentDate = new Date();
-		sessionLog = `Log Start for ${sessionFooter}: ${currentDate.getFullYear()}/${
-			currentDate.getMonth() + 1
-		}/${currentDate.getDate()} @ ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}\r\n\r\n`;
-		logDate = currentDate;
-		term.focus();
-	}
-	return false;
-}
-
-// draw/re-draw menu and reattach listeners
-// when dom is changed, listeners are abandonded
-function drawMenu() {
-	logBtn.addEventListener("click", toggleLog);
-	if (loggedData) {
-		downloadLogBtn.addEventListener("click", downloadLog);
-		downloadLogBtn.style.display = "block";
-	}
-	adjustBtn.innerHTML = "↔️ Adjust Window";
-	adjustBtn.style.display = "block";
-	adjustBtn.addEventListener("click", doResize(colsInput.value, rowsInput.value));
-}
-
-function doResize(cols, rows) {
-	colsInput.value = "0";
-	rowsInput.value = "0";
-	term.clear();
-	console.log("resizing window");
-	socket.emit("resize", { cols: Number(cols), rows: Number(rows) });
-	return true;
-}
-
 function resizeScreen() {
 	fitAddon.fit();
 	socket.emit("resize", { cols: term.cols, rows: term.rows });
@@ -129,9 +37,6 @@ term.onData((data) => {
 
 socket.on("data", (data) => {
 	term.write(data);
-	if (sessionLogEnable) {
-		sessionLog += data;
-	}
 });
 
 socket.on("connect", () => {
@@ -144,10 +49,6 @@ socket.on("setTerminalOpts", (data) => {
 
 socket.on("title", (data) => {
 	document.title = data;
-});
-
-socket.on("menu", () => {
-	drawMenu();
 });
 
 socket.on("status", (data) => {
--- server/socket.js
+++ server/socket.js
@@ -1,16 +1,9 @@
-/* eslint-disable complexity */
-/* eslint no-unused-expressions: ["error", { "allowShortCircuit": true, "allowTernary": true }],
-   no-console: ["error", { allow: ["warn", "error"] }] */
-/* jshint esversion: 6, asi: true, node: true */
-// socket.js
-
 // private
 const SSH = require("ssh2").Client;
 const CIDRMatcher = require("cidr-matcher");
 const validator = require("validator");
 const dnsPromises = require("dns").promises;
 const util = require("util");
-const { auditLog, logError } = require("./logging");
 
 /**
  * parse conn errors
@@ -29,7 +22,6 @@ function connError(socket, err) {
 	if (err?.level === "client-timeout") {
 		msg = `Connection Timeout: ${session.ssh.host}`;
 	}
-	logError(socket, "CONN ERROR", msg);
 }
 
 /**
@@ -43,11 +35,6 @@ async function checkSubnet(socket) {
 			const result = await dnsPromises.lookup(socket.request.session.ssh.host);
 			ipaddress = result.address;
 		} catch (err) {
-			logError(
-				socket,
-				"CHECK SUBNET",
-				`${err.code}: ${err.hostname} user=${socket.request.session.username} from=${socket.handshake.address}`
-			);
 			socket.emit("ssherror", "404 HOST IP NOT FOUND");
 			socket.disconnect(true);
 			return;
@@ -56,11 +43,6 @@ async function checkSubnet(socket) {
 
 	const matcher = new CIDRMatcher(socket.request.session.ssh.allowedSubnets);
 	if (!matcher.contains(ipaddress)) {
-		logError(
-			socket,
-			"CHECK SUBNET",
-			`Requested host ${ipaddress} outside configured subnets / REJECTED user=${socket.request.session.username} from=${socket.handshake.address}`
-		);
 		socket.emit("ssherror", "401 UNAUTHORIZED");
 		socket.disconnect(true);
 	}
@@ -72,10 +54,6 @@ module.exports = function appSocket(socket) {
 
 	socket.once("disconnecting", (reason) => {
 		if (login === true) {
-			auditLog(
-				socket,
-				`LOGOUT user=${socket.request.session.username} from=${socket.handshake.address} host=${socket.request.session.ssh.host}:${socket.request.session.ssh.port}`
-			);
 			login = false;
 		}
 	});
@@ -102,7 +80,6 @@ module.exports = function appSocket(socket) {
 
 		conn.on("handshake", (data) => {
 			socket.emit("setTerminalOpts", socket.request.session.ssh.terminal);
-			socket.emit("menu");
 			socket.emit("title", `ssh://${socket.request.session.ssh.host}`);
 			if (socket.request.session.ssh.header.background)
 				socket.emit("headerBackground", socket.request.session.ssh.header.background);
@@ -114,27 +91,16 @@ module.exports = function appSocket(socket) {
 		});
 
 		conn.on("ready", () => {
-			auditLog(
-				socket,
-				`LOGIN user=${socket.request.session.username} from=${socket.handshake.address} host=${socket.request.session.ssh.host}:${socket.request.session.ssh.port}`
-			);
 			login = true;
 			socket.emit("status", "SSH CONNECTION ESTABLISHED");
 			socket.emit("statusBackground", "green");
 			const { term, cols, rows } = socket.request.session.ssh;
 			conn.shell({ term, cols, rows }, (err, stream) => {
-				if (err) {
-					logError(socket, `EXEC ERROR`, err);
-					conn.end();
-					socket.disconnect(true);
-					return;
-				}
 				socket.once("disconnect", (reason) => {
 					conn.end();
 					socket.request.session.destroy();
 				});
 				socket.on("error", (errMsg) => {
-					logError(socket, "SOCKET ERROR", errMsg);
 					conn.end();
 					socket.disconnect(true);
 				});
@@ -149,15 +115,9 @@ module.exports = function appSocket(socket) {
 				});
 				stream.on("close", (code, signal) => {
 					if (socket.request.session?.username && login === true) {
-						auditLog(
-							socket,
-							`LOGOUT user=${socket.request.session.username} from=${socket.handshake.address} host=${socket.request.session.ssh.host}:${socket.request.session.ssh.port}`
-						);
 						login = false;
 					}
-					if (code !== 0 && typeof code !== "undefined")
-						logError(socket, "STREAM CLOSE", util.inspect({ message: [code, signal] }));
-					socket.disconnect(true);
+					if (code !== 0 && typeof code !== "undefined") socket.disconnect(true);
 					conn.end();
 				});
 				stream.stderr.on("data", (data) => {
@@ -167,11 +127,9 @@ module.exports = function appSocket(socket) {
 		});
 
 		conn.on("end", (err) => {
-			if (err) logError(socket, "CONN END BY HOST", err);
 			socket.disconnect(true);
 		});
 		conn.on("close", (err) => {
-			if (err) logError(socket, "CONN CLOSE", err);
 			socket.disconnect(true);
 		});
 		conn.on("error", (err) => connError(socket, err));
@@ -184,7 +142,6 @@ module.exports = function appSocket(socket) {
 			(socket.request.session.userpassword || socket.request.session.privatekey) &&
 			socket.request.session.ssh
 		) {
-			// console.log('hostkeys: ' + hostkeys[0].[0])
 			const { ssh } = socket.request.session;
 			ssh.username = socket.request.session.username;
 			ssh.password = socket.request.session.userpassword;
